3934a74e1fcc7d05f5c61ae4c0e9391629224998
Vitalii <crazzzyvio@gmail.com>
2024-06-19 10:51:56 +0300

    Remake controller and add delete method

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,22 +1,41 @@
 import Transaction from "../models/transaction.js";
 
-const getAllTransactions = async (req, res) => {
-  try {
-    const transactions = await Transaction.findAll();
-    res.status(200).json(transactions);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
+class Controllers {
+  getAllTransactions = async (req, res) => {
+    try {
+      const transactions = await Transaction.findAll();
+      res.status(200).json(transactions);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
 
-const addTransaction = async (req, res) => {
-  try {
-    const { type, comment, price } = req.body;
-    const newTransaction = await Transaction.create({ type, comment, price });
-    res.status(201).json(newTransaction);
-  } catch (error) {
-    res.status(500).send(error.message);
+  addTransaction = async (req, res) => {
+    try {
+      const { type, comment, price } = req.body;
+      const newTransaction = await Transaction.create({ type, comment, price });
+      res.status(201).json(newTransaction);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
+
+  deleteTransactionById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTransaction = await Transaction.findByPk(id);
+
+      if (deleteTransaction) {
+        await deleteTransaction.destroy();
+        res.status(204).json();
+      } else {
+        res.status(404).json({error: "Transaction not found"})
+      }
+      
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 }
 
-export default { getAllTransactions, addTransaction };
+export default Controllers;
--- router/router.js
+++ router/router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import controllers from "../controllers/controllers.js";
+import Controllers from "../controllers/controllers.js";
 
 const router = Router();
+const controller = new Controllers();
 
 router.get("/", (req, res) => {
   res.send("Hello, go to /transactions")
 })
 
-router.get("/transactions", controllers.getAllTransactions);
-router.post("/transactions", controllers.addTransaction);
+router.get("/transactions", controller.getAllTransactions);
+router.post("/transactions", controller.addTransaction);
+router.delete("/transactions/:id", controller.deleteTransactionById);
 
 export default router;
